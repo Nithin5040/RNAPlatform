7d866f240c209bb8fe15a95e579238460187f314
Praveen M <Praveen@vishvin.co.in>
2026-07-22 18:15:05 +0530

    updated the mobile api

--- BackEnd/Routes/AllRoutes.js
+++ BackEnd/Routes/AllRoutes.js
@@ -18,25 +18,10 @@ const applyRoutes = (app) => {
 
     //Driver Creation
     app.use(backendHelpers.DRIVER_CREATION, driverCreation);
-    app.use(backendHelpers.DRIVER_LOGIN,driverlogin)
-
-
+    app.use(backendHelpers.DRIVER_LOGIN, driverlogin)
     //AssignRoute
     app.use(backendHelpers.ASSIGN_ROUTE, AssignRoute)
-
-
-
-
-
-
 }
 
 
 export default applyRoutes;
-
-
-
-
-
-
-
