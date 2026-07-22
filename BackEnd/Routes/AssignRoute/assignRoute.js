03caf0c11cf17a33bc6ab489fe90a84d1c524d4f
Praveen M <Praveen@vishvin.co.in>
2026-07-22 13:45:57 +0530

    updated the mobile api

--- BackEnd/Routes/AssignRoute/assignRoute.js
+++ BackEnd/Routes/AssignRoute/assignRoute.js
@@ -1,5 +1,6 @@
 import express from "express"
-import {fetchAssignDpdwn,insertAssigneRoutesForDriver, fetchAssignRouteDetails} from "../../Controllers/AssignRoute/AssignController.js"
+import {fetchAssignDpdwn,insertAssigneRoutesForDriver, fetchAssignRouteDetails, submitStationDetail} from "../../Controllers/AssignRoute/AssignController.js"
+import {stationUpload} from "../../Middlewares/StationDetails/stationdetail.js"
 
 const router = express.Router()
 
@@ -8,9 +9,14 @@ router.post("/fecthAssignDpdwn", fetchAssignDpdwn)
 router.post("/insertAssigndriver", insertAssigneRoutesForDriver)
 
 
+
+//this are the mobile apis========================================
 //this is the fecthing the driver details when the driver login ok 
 
 router.post("/fetchAssignRouteDetails", fetchAssignRouteDetails)
+
+router.post("/submitStationDetail",  stationUpload.array("files", 20), submitStationDetail)
+// ==================================================================
 
 
 
