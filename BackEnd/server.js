fbe78a3191905cb0c922d82330df287d23f356c9
Nithin T B <nithintbtke@gmail.com>
2026-07-24 10:09:44 +0530

    updated

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,27 +1,23 @@
 import express from "express";
-const app = express();
-import cors from "cors"
-import { connectDb } from "./Config/DbConfig.js";
-import applyRoutes from "./Routes/AllRoutes.js"
+import cors from "cors";
 import dotenv from "dotenv";
-import { json } from "body-parser";
+import { connectDb } from "./Config/DbConfig.js";
+import applyRoutes from "./Routes/AllRoutes.js";
+
 dotenv.config();
 
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
 
 app.use(cors({
   origin: ["http://localhost:5173", "http://localhost:5174"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  exposedHeaders: ["Content-Disposition", "content-type"],
   credentials: true
 }));
 
+await connectDb();
 
 applyRoutes(app);
 
-
-app.listen(process.env.PORT, async () => {
-  await connectDb()
-  console.log(`server is running on port ${process.env.PORT}.....`);
-})
+export default app;
