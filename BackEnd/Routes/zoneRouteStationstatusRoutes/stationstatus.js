ac9be5c9c3a56daac8e0d6d54a2c481db30c0ff2
Nithin T B <nithintbtke@gmail.com>
2026-07-30 15:17:42 +0530

    updated

--- BackEnd/Routes/zoneRouteStationstatusRoutes/stationstatus.js
+++ BackEnd/Routes/zoneRouteStationstatusRoutes/stationstatus.js
@@ -1,9 +1,13 @@
-import { zoneDrpDwn,assignRouteStatuscount } from "../../Controllers/ZoneRouteStationStatus/zone.js";
+import { zoneDrpDwn, assignRoutescount, assignRootFetchbasedonZoneMaster, substationCountBasedonAssignedRoot } from "../../Controllers/ZoneRouteStationStatus/zone.js";
 import express from "express";
-const router=express.Router();
+const router = express.Router();
 
 
-router.post("/zoneDrpDwn",zoneDrpDwn);
-router.post("/assignRouteStatuscount",assignRouteStatuscount)
+router.post("/zoneDrpDwn", zoneDrpDwn);
+router.post("/assignRoutecount", assignRoutescount);
+router.post("/assignRootFetchbasedonZoneMaster", assignRootFetchbasedonZoneMaster)
+
+router.post("/substationCountBasedonAssignedRoot", substationCountBasedonAssignedRoot)
+
 
 export default router;
