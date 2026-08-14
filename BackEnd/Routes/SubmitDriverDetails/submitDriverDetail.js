e58a0d7026d0b141aa26ab14e2f781bc1e6bfbb1
Nithin T B <nithintbtke@gmail.com>
2026-08-14 12:08:23 +0530

    fix: add missing route /fetchDriverDetailDropdwon to submitDriverDetail routes

--- BackEnd/Routes/SubmitDriverDetails/submitDriverDetail.js
+++ BackEnd/Routes/SubmitDriverDetails/submitDriverDetail.js
@@ -5,5 +5,7 @@ import { fetchDriverSubmittedDetails } from "../../Controllers/SubmittedDriverDe
 const router = express.Router()
 
 router.post("/fetchDriverSubmittedDetails", fetchDriverSubmittedDetails)
+router.post("/fetchDriverDetailDropdwon", fetchDriverSubmittedDetails)
+router.post("/fetchDriverSubmittedDpdn", fetchDriverSubmittedDetails)
 
 export default router
